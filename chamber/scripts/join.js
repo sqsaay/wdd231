d96a76d0db48bfb546930d33a6681976e7324769
sqsaay <ayeleth.quevedo@gmail.com>
2026-02-07 22:47:21 -0600

    v04 - Join Page

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -9,12 +9,11 @@ const buttonData = [
 async function getMembership() {
     const response = await fetch('../chamber/data/memberships.json');
     const data = await response.json();
-    console.table(data.memberships);
     displayMemberships(data.memberships);
+    return data.memberships;
 }
 
 getMembership();
-
 const displayMemberships = (memberships) => {
     memberships.forEach(member => {
         const card = document.createElement("section");
@@ -30,6 +29,52 @@ const displayMemberships = (memberships) => {
         card.appendChild(mButton);
 
         cards.appendChild(card);
-        
+
     });
 };
+
+
+
+/*Modals*/
+const modalNp = document.createElement("dialog");
+modalNp.id = "modalNp";
+function createModals(modals) {
+    
+    modals.forEach(data => {
+    const title = document.createElement("h2");
+    const eligibility = document.createElement("p");
+    const fee = document.createElement("p");
+    const perks = document.createElement("ul");
+    const closeBtn = document.createElement("button");
+
+        title.textContent = `${membership_type}`;
+        eligibility.textContent = `Fee: ${eligibility}`;
+        fee.textContent = `Fee: ${fee}`;
+        closeBtn.id = "closeModal";
+        closeBtn.textContent = "close";
+
+        closeBtn.addEventListener("click", ()=>{
+            modalNp.close();
+        });
+        modalNp.appendChild(title);
+        modalNp.appendChild(eligibility);
+        modalNp.appendChild(fee);
+        modalNp.appendChild(closeBtn);
+    });
+}
+
+    const dataMembership = getMembership();
+    create(dataMembership);
+    console.log(dataMembership);
+
+    function create (members){
+        members.forEach( member =>{
+            console.log(member[0])
+        });
+
+    }
+
+
+
+
+
